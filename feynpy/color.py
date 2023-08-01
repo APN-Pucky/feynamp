db54d566f4795c357e6080286d1f7e7a02aad755
Alexander Puck Neuwirth <alexander@neuwirth-informatik.de>
2023-08-01 22:38:04 +0200

    really bad performance, but computed colors :)

--- feynpy/color.py
+++ feynpy/color.py
@@ -1,14 +1,9 @@
-import itertools
+from re import I
 
-from sympy import Function, Wild, symbols
-from sympy.tensor.tensor import (
-    TensorHead,
-    TensorIndex,
-    TensorIndexType,
-    WildTensorIndex,
-    tensor_heads,
-    tensor_indices,
-)
+from sympy import Dummy, Function, I, Wild, cacheit, symbols
+
+# from symengine import *
+
 
 N_c = symbols("N_c", integer=True)
 N_g = symbols("N_g", integer=True)
@@ -16,28 +11,100 @@ C_A = symbols("C_A", integer=True)
 C_F = symbols("C_F", integer=True)
 T_F = symbols("T_F", integer=True)
 
-ColorIndex = TensorIndexType(
-    "Color", dummy_name="C", dim=N_c, metric_symmetry=0
-)  # Bug: None crashes replacements
-GluonIndex = TensorIndexType(
-    "Gluon", dummy_name="G", dim=N_g, metric_symmetry=0
-)  # Bug: None crashes replacements
-
-T = TensorHead("T", [GluonIndex, ColorIndex, ColorIndex])
-f = TensorHead("f", [GluonIndex, GluonIndex, GluonIndex])
-
 T = Function("T")
 f = Function("f")
-delta = Function("delta")
+
+delta_c = Function("delta_c")
+delta_g = Function("delta_g")
+
+
+def apply_color(expr):
+    expr = apply_id(expr)
+    expr = apply_f(expr)
+    expr = apply_TT(expr)
+    expr = apply_delta(expr)
+    expr = apply_T(expr)
+    expr = apply_id(expr)
+    return expr
 
 
 def apply_id(expr):
-    i = TensorIndex(True, ColorIndex)
-    j = TensorIndex(True, GluonIndex)
-    expr = expr.subs(ColorIndex.delta(-i, i), ColorIndex.dim)
-    expr = expr.subs(ColorIndex.delta(i, -i), ColorIndex.dim)
-    expr = expr.subs(GluonIndex.delta(-j, j), GluonIndex.dim)
-    expr = expr.subs(GluonIndex.delta(j, -j), GluonIndex.dim)
+    wi = Wild("wi")
+    expr = expr.replace(delta_c(wi, wi), N_c)
+    expr = expr.replace(delta_g(wi, wi), N_g)
+    return expr
+
+
+def apply_delta(expr, expand=True):
+    wa, wb, wc, wd, wi, wj, wk, ww = symbols("wa wb wc wd wi wj wk ww", cls=Wild)
+    (
+        d1,
+        d2,
+        d3,
+        d4,
+        d5,
+        d6,
+        d7,
+        d8,
+    ) = symbols("d1 d2 d3 d4 d5 d6 d7 d8", cls=Dummy)
+    if expand:
+        expr = expr.expand()  # so that we hit it
+
+    expr = expr.replace(delta_c(wa, wb) * delta_c(wb, wc) * ww, delta_c(wa, wc) * ww)
+    expr = expr.replace(delta_c(wa, wb) * delta_c(wc, wb) * ww, delta_c(wa, wc) * ww)
+    expr = expr.replace(delta_c(wb, wa) * delta_c(wb, wc) * ww, delta_c(wa, wc) * ww)
+    expr = expr.replace(delta_c(wb, wa) * delta_c(wc, wb) * ww, delta_c(wa, wc) * ww)
+
+    expr = expr.replace(ww * T(wa, wi, wj) * delta_c(wi, wk), ww * T(wa, wk, wj))
+    expr = expr.replace(ww * T(wa, wj, wi) * delta_c(wi, wk), ww * T(wa, wj, wk))
+    expr = expr.replace(ww * T(wa, wi, wj) * delta_c(wk, wi), ww * T(wa, wk, wj))
+    expr = expr.replace(ww * T(wa, wj, wi) * delta_c(wk, wi), ww * T(wa, wj, wk))
+
+    expr = expr.replace(ww * T(wa, wi, wj) * delta_g(wa, wb), ww * T(wb, wi, wj))
+    expr = expr.replace(ww * T(wa, wi, wj) * delta_g(wb, wa), ww * T(wb, wi, wj))
+
+    # expr = expr.replace(ww*T(wa,wi,wj)* delta_c(wi,wj), ww*T(wa,d1,d1))
+    # expr = expr.replace(ww*T(wa,wi,wj)* delta_c(wj,wi), ww*T(wa,d2,d2))
+
+    expr = expr.replace(ww * f(wa, wb, wc) * delta_g(wa, wd), ww * f(wd, wb, wc))
+    expr = expr.replace(ww * f(wa, wb, wc) * delta_g(wb, wd), ww * f(wa, wd, wc))
+    expr = expr.replace(ww * f(wa, wb, wc) * delta_g(wc, wd), ww * f(wa, wb, wd))
+
+    expr = expr.replace(ww * f(wa, wb, wc) * delta_g(wd, wa), ww * f(wd, wb, wc))
+    expr = expr.replace(ww * f(wa, wb, wc) * delta_g(wd, wb), ww * f(wa, wd, wc))
+    expr = expr.replace(ww * f(wa, wb, wc) * delta_g(wd, wc), ww * f(wa, wb, wd))
+
+    expr = expr.replace(ww * f(wa, wb, wc) * delta_c(wa, wb), ww * f(d3, d3, wc))
+    expr = expr.replace(ww * f(wa, wb, wc) * delta_c(wa, wc), ww * f(d4, wb, d4))
+    expr = expr.replace(ww * f(wa, wb, wc) * delta_c(wb, wc), ww * f(wa, d5, d5))
+
+    expr = expr.replace(ww * f(wa, wb, wc) * delta_c(wb, wa), ww * f(d6, d6, wc))
+    expr = expr.replace(ww * f(wa, wb, wc) * delta_c(wc, wa), ww * f(d7, wb, d7))
+    expr = expr.replace(ww * f(wa, wb, wc) * delta_c(wc, wb), ww * f(wa, d8, d8))
+    return expr
+
+
+def apply_T(expr):
+    wa, wi = symbols("wa wi", cls=Wild)
+    # traceless
+    expr = expr.replace(T(wa, wi, wi), 0)
+    return expr
+
+
+def apply_f(expr):
+    wa, wb, wc, ww = symbols("wa wb wc ww", cls=Wild)
+    i, j, k = symbols("i j k", cls=Dummy)
+    dict = expr.match(ww * f(wa, wb, wc))
+    expr = expr.replace(
+        f(wa, wb, wc) * ww,
+        2
+        * I
+        * (
+            T(wc, i, j) * T(wb, j, k) * T(wa, k, i)
+            - T(wa, i, j) * T(wb, j, k) * T(wc, k, i)
+        )
+        * ww,
+    )
     return expr
 
 
@@ -48,26 +115,20 @@ def apply_TT(expr):
     ========
 
     >>> from feynpy.color import *
-    >>> i,j,k,l = tensor_indices('i,j,k,l',ColorIndex)
-    >>> g = tensor_indices('g',GluonIndex)
-    >>> m = T(g,i,j)* T(-g,k,l)
+    >>> i,j,k,l,g = symbols('i j k l g')
+    >>> m = T(g,i,j)*T(g,k,l)
     >>> apply_TT(m)
     >>> m
     -C_A*Color.delta(i, k)*Color.delta(j, l)/2 + Color.delta(i, l)*Color.delta(j, k)/2
 
     """
-    i = WildTensorIndex("i", ColorIndex, ignore_updown=True)
-    j = WildTensorIndex("j", ColorIndex, ignore_updown=True)
-    k = WildTensorIndex("k", ColorIndex, ignore_updown=True)
-    l = WildTensorIndex("l", ColorIndex, ignore_updown=True)
-    t = WildTensorIndex("t", GluonIndex, ignore_updown=True)
-    rem = Wild("rem")
+    wi, wj, wk, wl, wg, ww = symbols("wi wj wk wl wg ww", cls=Wild)
     expr = expr.replace(
-        rem * T(t, i, j) * T(-t, l, k),
-        rem
-        * (
-            ColorIndex.delta(i, l) * ColorIndex.delta(k, j) / 2
-            - 1 / N_c / 2 * ColorIndex.delta(i, j) * ColorIndex.delta(k, l)
-        ),
+        T(wg, wi, wj) * T(wg, wk, wl) * ww,
+        (
+            delta_c(wi, wl) * delta_c(wk, wj) / 2
+            - 1 / N_c / 2 * delta_c(wi, wj) * delta_c(wk, wl)
+        )
+        * ww,
     )
     return expr
--- tests/test_color.py
+++ tests/test_color.py
@@ -2,8 +2,37 @@ from feynpy.color import *
 
 
 def test_apply_TT():
-    i, j, k, l = tensor_indices("i,j,k,l", ColorIndex)
-    g = tensor_indices("g", GluonIndex)
-    m = 5 * T(g, i, j) * T(-g, k, l)
-    l = apply_TT(m)
-    assert l == 5
+    i, j, k, l, g = symbols("i j k l g")
+    m = T(g, i, j) * T(g, k, l)
+    result = apply_TT(m)
+    expected = delta_c(i, l) * delta_c(k, j) / 2 - delta_c(i, j) * delta_c(k, l) / (
+        2 * N_c
+    )
+    assert expected.equals(result)
+
+
+def test_apply_TT_scaled():
+    i, j, k, l, g = symbols("i j k l g")
+    m = T(g, i, j) * T(g, k, l) * 5
+    result = apply_TT(m)
+    expected = (
+        delta_c(i, l) * delta_c(k, j) / 2 * 5
+        - delta_c(i, j) * delta_c(k, l) / (2 * N_c) * 5
+    )
+    assert expected.equals(result)
+
+
+def test_fTT():
+    a, b, c, i, j, k, l, g = symbols("a b c i j k l g")
+    test = f(a, b, c) * T(c, i, j) * T(b, k, i)
+    result = apply_color(apply_color(apply_color(test)))
+    expected = I / 2 * N_c * T(a, k, j)
+    assert expected.equals(result)
+
+
+def test_TTT():
+    a, b, c, i, j, k, l, g = symbols("a b c i j k l g")
+    test = T(b, j, i) * T(b, l, k) * T(a, k, j)
+    result = apply_color(apply_color(test))
+    expected = -T(a, l, i) / 2 / N_c
+    assert expected.equals(result)
